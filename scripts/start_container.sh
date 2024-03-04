c9b0848042001aa46d4895006ebd97b18f9d4948
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-04 18:57:12 +0500

    env values added

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ACCOUNT_ID=099199746132
+export REPOSITORY_URI=099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-project-ecr-0001
+
 # Login , authenticate your Docker client to your registry
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
