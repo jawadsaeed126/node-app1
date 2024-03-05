99eeb3ccab82f2b0cab3365581c03a0d806e9477
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-05 11:11:57 +0500

    using docker-compose up

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -9,8 +9,9 @@ export REPOSITORY_URI=099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-project-
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from ECR
 docker pull $REPOSITORY_URI:latest
 
 # Run the Docker image as a container
-docker run -d -p 3000:3000 $REPOSITORY_URI:latest
+# docker run -d -p 3000:3000 $REPOSITORY_URI:latest
+docker-compose up -d --build
