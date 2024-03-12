9a9b7270c8ef9158becee57208b1113b0bb4fa8f
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-12 15:51:31 +0500

    files updated

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 export AWS_DEFAULT_REGION=eu-west-1
 export AWS_ACCOUNT_ID=099199746132
-export REPOSITORY_URI=099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-project-ecr-0001
+export REPOSITORY_URI=099199746132.dkr.ecr.eu-west-1.amazonaws.com/private-ecr-repository-app1-node-api1
 
 # Login , authenticate your Docker client to your registry
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
@@ -14,6 +14,6 @@ docker pull $REPOSITORY_URI:latest
 
 
 # Run the Docker image as a container
-# docker run -d -p 3000:3000 $REPOSITORY_URI:latest
-cd /home/ubuntu/project-root/node-application1
-docker-compose up -d
+docker run -d -p 3000:3000 $REPOSITORY_URI:latest
+# cd /home/ubuntu/project-root/node-application1
+# docker-compose up -d
